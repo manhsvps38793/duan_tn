49df56a1953c5bfa29a7a1607e4c86f402187031
Sầm Văn Mạnh <manhsvps38793@gmail.com>
2025-06-01 19:40:33 +0700

    layvelayve

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,70 +1,52 @@
- <footer class="footer">
-        <div class="grid wide container">
-            <div class="row">
-                <div class="col l-3 m-3 c-12 ">
-                    <div class="box-footer">
-                        <div class="box-footer_logo"><a href="/index.html">M A G</a></div>
-                        <p>M A G
-                            Giấy chứng nhận đăng ký HKD số 17A80041952 do Phòng Tài chính - Kế hoạch, Uỷ ban nhân dân thành phố HCM cấp ngày 19/10/2004
-                            Địa chỉ: Số 391/Tô ký, Phường Trung Mỹ Tây, Thành phố HCM, Tỉnh HCM, Việt Nam <br>
-                            Email: manhsvps38793@gmail.com <br>
-                            Điện thoại: 0325132746
-                        </p>
-                    </div>
-                </div>
-                <div class="col l-3 m-3 c-12 ">
-                    <div class="box-footer">
-                            <h3>Theo dõi</h3>
-                            <p>
-                                Theo dõi M A G qua các nền tảng để nhận khuyến mãi nhé
-                            </p>
-                            <div class="grid wide container">
-                                <div class="row icon-center">
-                                    <div class="col l-2 c-2">
-                                        <div class="footer-icon"><img src="img/facebook.svg" alt=""></div>
-                                    </div>
-                                    <div class="col l-2 c-2">
-                                        <div class="footer-icon"><img src="img/instagram.svg" alt=""></div>
-                                    </div>
-                                    <div class="col l-2 c-2">
-                                        <div class="footer-icon"><img src="img/lazadaico.webp" alt=""></div>
-                                    </div>
-                                    <div class="col l-2 c-2">
-                                        <div class="footer-icon"><img src="img/shopeeico.webp" alt=""></div>
-                                    </div>
-                                    <div class="col l-2 c-2">
-                                        <div class="footer-icon"><img src="img/tiktok.svg" alt=""></div>
-                                    </div>
-                                </div>
-                            </div>
-                    </div>
-                </div>
-                <div class="col l-3 m-3 c-12 ">
-                    <div class="box-footer">
-                        <h3>Về chúng tôi</h3>
-                        <ul>
-
-                            <li><a href="/index.html">Trang chủ</a></li>
-                            <li><a href="/pruduct.html">Tất cả sản phẩm</a></li>
-                            <li><a href="/form.html">Bảng Size</a></li>
-                            <li><a href="">Kiểm tra đơn hàng</a></li>
-                            <li><a href="">Hệ Thống Cửa Hàng</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col l-3 m-3 c-12 ">
-                    <div class="box-footer">
-                        <h3>Chính sách</h3>
-                        <ul>
-                            <li><a href="">Chính sách mua hàng</a></li>
-                            <li><a href="">Chính sách bảo mật</a></li>
-                            <li><a href="">Phương thức thanh toán</a></li>
-                            <li><a href="">Chính sách giao nhận, vận chuyển, kiểm hàng</a></li>
-                            <li><a href="">Chính sách đổi trả</a></li>
-                        </ul>
-                    </div>
+  <footer>
+        <div class="footer-container">
+            <div class="footer-col footer-about">
+                <h3>M A G</h3>
+                <p>M A G là tạp chí thời trang cao cấp, cung cấp những tin tức mới nhất về xu hướng, phong cách và các sự kiện thời trang đình đám.</p>
+                <div class="footer-social">
+                    <a href="#" aria-label="Facebook"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#" aria-label="Twitter"><i class="fab fa-twitter"></i></a>
+                    <a href="#" aria-label="Instagram"><i class="fab fa-instagram"></i></a>
+                    <a href="#" aria-label="Pinterest"><i class="fab fa-pinterest-p"></i></a>
                 </div>
             </div>
+
+            <div class="footer-col">
+                <h3>Liên kết nhanh</h3>
+                <ul class="footer-links">
+                    <li><a href="#">Trang chủ</a></li>
+                    <li><a href="#">Sản phẩm</a></li>
+                    <li><a href="#">Về chúng tôi</a></li>
+                    <li><a href="#">Tin tức</a></li>
+                    <li><a href="#">Liên hệ</a></li>
+                </ul>
+            </div>
+
+            <div class="footer-col">
+                <h3>Danh mục</h3>
+                <ul class="footer-links">
+                    <li><a href="#">Áo thun</a></li>
+                    <li><a href="#">Áo Polo</a></li>
+                    <li><a href="#">Áo sơ mi</a></li>
+                    <li><a href="#">Áo khoác</a></li>
+                    <li><a href="#">Quần</a></li>
+                    <li><a href="#">Phụ kiện</a></li>
+                </ul>
+            </div>
+
+            <div class="footer-col footer-contact">
+                <h3>Liên hệ</h3>
+                <ul>
+                    <li><i class="fas fa-map-marker-alt"></i> 391 Đường Tô ký, Quận 12, TP.HCM</li>
+                    <li><i class="fas fa-phone"></i> 0325132746</li>
+                    <li><i class="fas fa-envelope"></i> manhsvps38793@gmail.com</li>
+                    <li><i class="fas fa-clock"></i> Thứ 2 - Thứ 6: 9:00 - 18:00</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="footer-bottom">
+            <p>&copy; 2023 M A G. Tất cả quyền được bảo lưu.</p>
         </div>
     </footer>
     <script>
